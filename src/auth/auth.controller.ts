ba563de66c6db6c10d74fcfc4d034b1d5b810cb6
Fernando <manuelhola9@gmail.com>
2024-03-31 02:02:17 -0700

    configuracion por roles realizada

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,13 @@
-import { Controller, Post, Body, Get, UseGuards} from '@nestjs/common';
+import { Controller, Post, Body, Get, UseGuards, SetMetadata} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreatUserDto, LoginUserDto } from './dto';
 import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from './decorators/role-protected/get-user.decorator';
+import { User } from './entities/user.entity';
+import { UserRoleGuard } from './guards/user-role/user-role.guard';
+import { RoleProtected } from './decorators/role-protected/role-protected.decorator';
+import { ValidRoles } from './interface';
+import { Auth } from './decorators/role-protected';
 
 @Controller('auth')
 export class AuthController {
@@ -20,11 +26,46 @@ export class AuthController {
 
   @Get('private')
   @UseGuards(AuthGuard())
-  testPrivate() {
+  testPrivate(
+
+    @GetUser() user: User
+
+  ) {
+
+    // console.log(user);
+
+
     return {
       message: 'This is a private route',
       ok: true,
-      user: {name: 'Fernado'}
+      user,
+    }
+  }
+
+
+  // @SetMetadata('roles', ['admin'])
+  
+  @Get('private2')
+  @UseGuards(AuthGuard(), UserRoleGuard)
+  @RoleProtected(ValidRoles.admin, ValidRoles.doctor)
+  private2(
+    @GetUser() user: User
+  ) {
+    return {
+      ok: true,
+      user,
+    }
+  }
+
+
+  @Get('private3')
+  @Auth(ValidRoles.admin)
+  private3(
+    @GetUser() user: User
+  ) {
+    return {
+      ok: true,
+      user,
     }
   }
 }
---
+++ src/auth/interface/index.ts
@@ -0,0 +1,2 @@
+export { JwtPayload } from './jwt-paylot.interface'
+export { ValidRoles } from './valid-roles'
