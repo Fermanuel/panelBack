181b401d586d9f18788dd31a5afadc890bd0b788
Fernando <manuelhola9@gmail.com>
2024-04-04 14:44:22 -0700

    modulo de pacientes creado

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
+import { PacientesModule } from './pacientes/pacientes.module';
 
 @Module({
   imports: [
@@ -17,6 +18,7 @@ import { AuthModule } from './auth/auth.module';
       synchronize: true,
     }),
     AuthModule,
+    PacientesModule,
   ],
 })
 export class AppModule {}
