2479034234fd46e29482a9c7952c10ae5de1d3ac
Fernando <manuelhola9@gmail.com>
2024-04-04 15:28:11 -0700

    creacion de la entidad paciente

--- src/pacientes/entities/paciente.entity.ts
+++ src/pacientes/entities/paciente.entity.ts
@@ -1,1 +1,51 @@
-export class Paciente {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Paciente {
+    
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column('text', {
+        unique: true,
+    })
+    NoControl: string;
+
+    @Column('text')
+    Nombre: string;
+
+    @Column('text')
+    ApellidoPaterno: string;
+
+    @Column('text')
+    ApellidoMaterno: string;
+
+    @Column('date')
+    cumple: Date;
+
+    @Column('text', {
+        nullable: true,
+    })
+    direccion: string;
+
+    @Column('text', {
+        unique: true,
+    })
+    telefono: string;
+
+    @Column('text', {
+        unique: true,
+    })
+    correoTec: string;
+
+    @Column('text', {
+        unique: true,
+        nullable: true,
+    })
+    correoPer: string;
+
+    @Column('date', {
+        default: () => 'CURRENT_TIMESTAMP',
+    })
+    created_at: Date;
+}
